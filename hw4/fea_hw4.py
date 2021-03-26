7ca7bb25bfa4c52cd1d6f200cb7e99ceae9dd403
yunqingjia <yjia16@outlook.com>
2021-03-25 21:19:54 -0400

    implemented finite element method from scratch (2D) with some test cases

---
+++ hw4/fea_hw4.py
@@ -0,0 +1,237 @@
+'''
+Spring 2021 AE 4132 HW4: finite element program implementation from scratch
+author: yjia67
+last updated: 03-24-2021
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import cmd
+
+class FEAHW4():
+
+    def __init__(self):
+        pass
+
+    def read_input(self, filename: str):
+        '''
+        Parse the input text file into matrices
+        Text file format:
+            nnodes
+            x_1 y_1 rx_1 ry_1 fx_1 fy_1
+            x_2 y_2 rx_2 ry_2 fx_2 fy_2
+            ...
+            x_n y_n rx_n ry_n fx_n fy_n
+            nels
+            n1_1 n2_1 E_1 A_1
+            n1_2 n2_2 E_2 A_2
+            ...
+            n1_n n2_n E_n A_n
+        nnodes:         # of nodes
+        (x_i, y_i):     coordinates of node i
+        (rx_i, ry_i):   constraints in the x and y directions of node i. 1=constrainted, 0=free
+        nels:           # of elements
+        (n1_i, n2_i):   the first node and second element of element i
+
+        Args:
+            - filename: name (and path) of the file to be parsed
+        Returns:
+            - nodes:    (nnodes, 6) np array containing nodal info
+            - els:      (nels, 4) np array containing elemental info
+        '''
+
+        f = open(filename, 'r')
+
+        # read nodes
+        nnodes = int(f.readline())
+        nodes = np.zeros((nnodes, 6))
+        for i in range(nnodes):
+            nodes[i] = list(map(float, f.readline().split()))
+
+        # read elements
+        nels = int(f.readline())
+        els = np.zeros((nels, 4))
+        for i in range(nels):
+            els[i] = list(map(float, f.readline().split()))
+
+        f.close()
+
+        print('Finished parsing.')
+        print('%d nodes and %d elements.' % (nnodes, nels))
+
+        return nodes, els
+
+    def solve_fea(self, nodes: np.ndarray, els: np.ndarray):
+        '''
+        Put everything together
+        Args:
+            - nodes:    (nnodes, 6) np array containing nodal info
+                        [x_i, y_i, rx_i, ry_i, fx_i, fy_i] for each node i
+            - els:      (nels, 4) np array containing elemental info
+                        [n1_i, n2_i, E_i, A_i] for each element i
+        Returns:
+            - u:        (nnodes, ) np array representing nodal displacement
+            - rxn:      (nnodes, ) np array representing reaction forces
+        '''
+        # extract variables for easy access
+        x, y, rx, ry, fx, fy = nodes.T
+
+        # compute stiffness matrix
+        k_global = self.compute_K(x, y, els)
+
+        # construct force vector
+        f = self.compute_F(fx, fy)
+
+        # apply boundary conditions
+        k_global_r, f_r, idx = self.apply_BC(rx, ry, k_global, f)
+
+        # compute displacement
+        u = np.linalg.inv(k_global_r) @ f_r
+        # add back in the nodes where displacement = 0
+        for i in idx:
+            u = np.insert(u, i, 0)
+
+        # solve for reaction forces
+        rxn = k_global @ u
+        return u, rxn
+
+    def compute_K(self, x: np.ndarray, y: np.ndarray, els: np.ndarray):
+        '''
+        Construct the global stiffness matrix without boundary conditions
+
+        Args:
+            - x:    (nnodes, ) np array containing x coord for each node
+            - y:    (nnodes, ) np array containing y coord for each node
+            - els:  (nels, 4) np array containing elemental info
+                    [n1_i, n2_i, E_i, A_i] for element i
+        Returns:
+            - k_global: (nnodes, nnodes) np array that represents
+                        the global stiffness matrix as follows
+                                    [u1_x, u1_y, ..., unnodes_x, unnodes_y]
+                        [u1_x]
+                        [u1_y]
+                         ...
+                        [unnodes_x]
+                        [unnodes_y]
+        '''
+
+        # loop through each element to construct the global stiffness matrix
+        k_global = np.zeros((2*len(nodes), 2*len(nodes)))
+        for i in range(len(els)):
+
+            n1, n2, E, A = els[i]
+            # convert to int for indexing, which starts at 0 lol
+            n1, n2 = int(n1-1), int(n2-1)
+
+            # compute elemental stiffness
+            x1, y1, x2, y2 = x[n1], y[n1], x[n2], y[n2]
+            l = np.sqrt((x2-x1)**2 + (y2-y1)**2) # elemental length
+            k= E*A/l
+            theta = np.arctan2(y2-y1, x2-x1)
+            R = self.compute_R(theta)
+            k_el = k*R
+
+            # assemble
+            i11, i12, i21, i22 = 2*n1, 2*n1+2, 2*n2, 2*n2+2
+            k_global[i11:i12, i11:i12] += k_el[:2, :2]
+            k_global[i11:i12, i21:i22] += k_el[:2, -2:]
+            k_global[i21:i22, i11:i12] += k_el[-2:, :2]
+            k_global[i21:i22, i21:i22] += k_el[-2:, -2:]
+
+        assert np.allclose(k_global, k_global.T, atol = 1e-03) # check symmetry
+
+        return k_global
+
+    def compute_F(self, fx: np.ndarray, fy: np.ndarray):
+        '''
+        Construct the force vector without boundary conditions
+        Args:
+            - fx:   (nnodes, ) np array containing force_x for each node
+            - fy:   (nnodes, ) np array containing force_y for each node
+        Returns:
+            - f:    (2*nnodes, 1) np array that represents the force vector
+                    [fx_1, fy_1, ..., fx_n, fy_n]
+        '''
+        f = np.zeros(2*len(fx))
+        f[::2] = fx
+        f[1::2] = fy
+        return f
+
+    def apply_BC(self, rx: np.ndarray, ry: np.ndarray, k_global: np.ndarray, f: np.ndarray):
+        '''
+        Apply displacement boundary conditions to both the stiffness matrix and the force vector
+        Args:
+            - nodes:    a size (nnodes, 6) numpy array containing nodal info
+                        [x_i, y_i, rx_i, ry_i, fx_i, fy_i] for node i
+            - k_global: the global stiffness matrix
+            - f:        the force vector
+        Returns:
+            - k_r:  reduced global stiffness matrix with the B.C. applied
+            - f_r:  reduced force vector with the B.C. applied
+            - idx:  indices where the displacement would be zero
+        '''
+
+        idx = []
+
+        for i in range(len(nodes)):
+            if rx[i] == 1:
+                idx.append(2*i)
+            if ry[i] == 1:
+                idx.append(2*i+1)
+
+
+        k_global = np.delete(k_global, obj=idx, axis=0)
+        k_global = np.delete(k_global, obj=idx, axis=1)
+        f = np.delete(f, idx)
+
+        k_global_r = k_global
+        f_r = f
+        
+        return k_global_r, f_r, idx
+
+    def compute_R(self, theta: float):
+        '''
+        Compute rotational matrix for a given theta
+        Args:
+            - theta: angle between the element coordinate and the global coordinate axes
+        Returns:
+            - R: the rotational matrix
+        '''
+        c = np.cos(theta)
+        s = np.sin(theta)
+        R = np.array([[ c**2,    c*s,  -c**2,   -c*s],
+                        [  c*s,   s**2,   -c*s,  -s**2],
+                        [-c**2,   -c*s,   c**2,    c*s],
+                        [ -c*s,  -s**2,    c*s,   s**2]])
+        return R
+
+if __name__ == '__main__':
+
+    hw4 = FEAHW4()
+
+    filename = input('Enter input filename (without the .txt extension): ')
+
+    # # Simply supported triangle case:
+    # filename = 'simp_triangle.txt'
+
+    # # HW4 Figure 1 Case 1:
+    # filename = 'hw4_case1.txt'
+
+    # # HW4 Figure 1 Case 2:
+    # filename = 'hw4_case2.txt'
+
+    try:
+        nodes, els = hw4.read_input(filename + '.txt')
+        u, rxn = hw4.solve_fea(nodes, els)
+
+        print('Nodal displacement vector:')
+        # [:, np.newaxis]
+        print(np.round(u,5))
+
+        print('Force vector including reactions:')
+        print(np.round(rxn,5))
+
+    except FileNotFoundError:
+        print('Error: file not found.')
+    except ValueError:
+        print('Error: input file format incorrect.')
